483e5610f28b3b85766781ee55a8af62e312af40
Dharmendra kumar <94430504+Dharmendrakumar16@users.noreply.github.com>
2024-03-21 22:39:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+// toogle icon bar 
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// slider
+
+var swiper = new Swiper(".Projects-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
+ 
+// scroll section 
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () =>{
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            // active navbar links
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    // sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active'); 
+}
